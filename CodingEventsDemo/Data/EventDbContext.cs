818baa206f42e707c0dec8c13e6cbf2dea668579
Ryan Chaffee <rchaff7@yahoo.com>
2020-12-23 21:20:57 -0600

    complete

--- CodingEventsDemo/Data/EventDbContext.cs
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -10,9 +10,10 @@ namespace CodingEventsDemo.Data
     public class EventDbContext : DbContext
     {
         public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> EventCategories { get; set; }
+
         public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
         {
-
         }
     }
 }
